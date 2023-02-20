05852b01ba7ca136efa065dcc0073a04dd958611
siddhesh021 <siddhesh021@gmail.com>
2023-02-20 12:54:50 +0530

    added loading functonality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react'
 import { useLocation,useNavigate } from 'react-router-dom'
 
 export default function Header() {
+
+  const [pageState, setpageState] = useState('Sign In')
   const  location = useLocation();
   const navigate = useNavigate();
+  const auth = getAuth()
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setpageState("Profile")
+      }else {
+        setpageState("Sign In")
+      }
+    })
+  })
+
   function pathMatch(route){
   if(route===location.pathname){
     return true
   }
 }
   return (
-    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
+    <div className='bg-white border-b shadow-sm sticky top-0 z-40'>
     <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
       <div>
         {/* <img src='https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg' alt='logo' 
@@ -22,7 +36,8 @@ export default function Header() {
           ${pathMatch("/") && "text-black border-b-red-500"}`} onClick={()=>navigate('/')}>Home</li>
 
           <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-          ${pathMatch("/sign-in") && "text-black border-b-red-500"}`} onClick={()=>navigate('/sign-in')}>Sign in</li>
+          ${(pathMatch("/sign-in") || pathMatch("/profile")) && "text-black border-b-red-500"}`} 
+          onClick={()=>navigate('/profile')}>{pageState}</li>
 
           <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
           ${pathMatch("/offers") && "text-black border-b-red-500"}`} onClick={()=>navigate('/offers')}>Offers</li>
